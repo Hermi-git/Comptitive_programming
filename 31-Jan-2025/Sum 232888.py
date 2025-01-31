19330bd9c63b8d5ce09c1538fdc8a4fd4e876045
Hermi-git <166540756+Hermi-git@users.noreply.github.com>
2025-02-01 01:15:08 +0300

    Created 'Sum' by A2SV Hub

---
+++ 31-Jan-2025/Sum 232888.py
@@ -0,0 +1,10 @@
+# Problem: Sum - https://codeforces.com/contest/1742/problem/A
+
+t = int(input())
+for _ in range(t):
+    a, b, c = map(int, input().split())
+    a, b, c = sorted([a, b, c])
+    if c == a+b:
+        print("YES")
+    else:
+        print("NO")
