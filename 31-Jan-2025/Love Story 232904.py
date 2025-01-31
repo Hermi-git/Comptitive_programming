3beb01d438294222fa530e2803ad1261c2b4e74c
Hermi-git <166540756+Hermi-git@users.noreply.github.com>
2025-02-01 01:35:53 +0300

    Created 'Love Story' by A2SV Hub

---
+++ 31-Jan-2025/Love Story 232904.py
@@ -0,0 +1,14 @@
+# Problem: Love Story - https://codeforces.com/contest/1829/problem/A
+
+t = int(input())
+compare_str = "codeforces"
+for _ in range(t):
+    s = input()
+    p1, p2 = 0, 0
+    differ_indicies = 0
+    while p1 < 10 and p2 < 10:
+        if s[p1] != compare_str[p2]:
+            differ_indicies += 1
+        p1 += 1
+        p2 += 1
+    print(differ_indicies)
