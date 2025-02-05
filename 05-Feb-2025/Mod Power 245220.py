9fb93718819f0c49e0dab392257a6979ee642b41
Hermi-git <166540756+Hermi-git@users.noreply.github.com>
2025-02-05 23:55:48 +0300

    Created 'Mod Power' by A2SV Hub

---
+++ 05-Feb-2025/Mod Power 245220.py
@@ -0,0 +1,8 @@
+# Problem: Mod Power - https://www.hackerrank.com/challenges/python-power-mod-power/problem?isFullScreen=true
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+a = int(input().strip())
+b = int(input().strip())
+m = int(input().strip())
+print(a ** b)
+print(a ** b % m)
