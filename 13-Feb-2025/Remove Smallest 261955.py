6d15b09aa26e22889dd88ad5af8239f073fe289f
Hermela Andargie <166540756+Hermi-git@users.noreply.github.com>
2025-02-13 09:50:47 +0300

    Created 'Remove Smallest' by A2SV Hub

---
+++ 13-Feb-2025/Remove Smallest 261955.py
@@ -0,0 +1,18 @@
+# Problem: Remove Smallest - https://codeforces.com/problemset/problem/1399/A
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    a = list(map(int, input().split()))
+    a.sort()
+    count = 0
+    for i in range(n-1):
+        if a[i+1] -a[i] == 1 or a[i+1] -a[i] == 0:
+            count += 1
+    if n - count == 1:
+        print("YES")
+    else:
+        print("NO")
+
+            
+       
