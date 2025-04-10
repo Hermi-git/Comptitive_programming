7b3a4b6d5033f22f071f5eade7dfa843c135ba84
Hermela Andargie <166540756+Hermi-git@users.noreply.github.com>
2025-04-10 19:40:41 +0300

    Created 'Is Graph Bipartite?' by A2SV Hub

---
+++ 10-Apr-2025/Is Graph Bipartite? 330942.py
@@ -0,0 +1,38 @@
+# Problem: Is Graph Bipartite? - https://leetcode.com/problems/is-graph-bipartite/
+
+class Solution:
+    def isBipartite(self, graph: List[List[int]]) -> bool:
+        black = -1
+        color = [black for _ in range(len(graph))]
+        visited = set()
+        def dfs(node):
+            for neigh in graph[node]:
+                if neigh in visited:
+                    if color[node] == color[neigh]:
+                        return False
+                else:
+                    if color[neigh] == -1:
+                        if color[node] == 0:
+                            color[neigh] = 1
+                        else:
+                            color[neigh] = 0
+                        visited.add(neigh)
+                        res = dfs(neigh)
+                        if not res:
+                            return False
+ 
+            return True
+
+        for node in range(len(graph)):
+            if color[node] == -1:
+                visited.add(node)
+                color[node] = 0
+                if not dfs(node):
+                    return False
+        
+        return True 
+        
+
+        
+                
+
