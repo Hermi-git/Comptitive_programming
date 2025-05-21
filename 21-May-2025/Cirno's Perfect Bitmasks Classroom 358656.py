ddcccebed83cfac00c9fb02d7921d23026bdf740
Hermela Andargie <166540756+Hermi-git@users.noreply.github.com>
2025-05-21 19:11:21 +0300

    Created 'Cirno's Perfect Bitmasks Classroom' by A2SV Hub

---
+++ 21-May-2025/Cirno's Perfect Bitmasks Classroom 358656.py
@@ -0,0 +1,16 @@
+# Problem: Cirno's Perfect Bitmasks Classroom - https://codeforces.com/problemset/problem/1688/A
+
+t = int(input())
+for _ in range(t):
+    x = int(input())
+    if x == 1:
+        print(3)
+    elif x % 2 != 0:
+        print(1)  
+    else:
+        low_bit = x & -x
+        if x ^ low_bit > 0:
+            print(low_bit)
+        else:
+            print(low_bit + 1)
+
