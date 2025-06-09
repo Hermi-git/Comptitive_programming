1f308ba85d7dc2a062fce1420d5692d9e7c5bf61
Hermela Andargie <166540756+Hermi-git@users.noreply.github.com>
2025-06-09 21:57:08 +0300

    Created 'D - Restricted Permutation' by A2SV Hub

---
+++ A2SV G6 - Round #13 09-Jun-2025/D - Restricted Permutation 365011.py
@@ -0,0 +1,30 @@
+# Problem: D - Restricted Permutation - https://codeforces.com/gym/607625/problem/D
+
+import heapq
+from collections import defaultdict
+n, m = map(int, input().split())
+graph = defaultdict(list)
+in_degree = [0] * (n + 1)
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    in_degree[b] += 1
+
+heap = []
+for i in range(1, n + 1):
+    if in_degree[i] == 0:
+        heapq.heappush(heap, i)
+
+ans = []
+while heap:
+    node = heapq.heappop(heap)
+    ans.append(node)
+    for neighbor in graph[node]:
+        in_degree[neighbor] -= 1
+        if in_degree[neighbor] == 0:
+            heapq.heappush(heap, neighbor)
+
+if len(ans) == n:
+    print(*ans)
+else:
+    print(-1)
