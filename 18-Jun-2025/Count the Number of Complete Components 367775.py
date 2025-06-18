654fa502f5adbb77ddb9ee4ef1209ee732be7c37
Hermela Andargie <166540756+Hermi-git@users.noreply.github.com>
2025-06-18 12:23:00 +0300

    Created 'Count the Number of Complete Components' by A2SV Hub

---
+++ 18-Jun-2025/Count the Number of Complete Components 367775.py
@@ -0,0 +1,33 @@
+# Problem: Count the Number of Complete Components - https://leetcode.com/problems/count-the-number-of-complete-components/
+
+class Solution:
+    def countCompleteComponents(self, n: int, edges: List[List[int]]) -> int:
+        graph = [[]for _ in range(n)]
+        for u, v in edges:
+            graph[u].append(v)
+            graph[v].append(u)
+        
+        visited = set()
+        def dfs(node, connected_nodes):
+            visited.add(node)
+            connected_nodes.append(node)
+            for neigh in graph[node]:
+                if neigh not in visited:
+                    dfs(neigh, connected_nodes)
+        
+        complete_nodes =0
+        for node in range(n):
+            if node not in visited:
+                connected_nodes = []
+                dfs(node, connected_nodes)
+                
+                total_edges= 0
+                for node in connected_nodes:
+                    total_edges += len(graph[node])
+                total_edges //= 2
+
+                m = len(connected_nodes)
+                if total_edges == m*(m-1)//2:
+                    complete_nodes += 1
+        return complete_nodes
+        
