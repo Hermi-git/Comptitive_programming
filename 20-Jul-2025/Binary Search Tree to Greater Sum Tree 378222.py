6e9454143f6654001b82109b96bf9d34db7909ad
Hermela Andargie <166540756+Hermi-git@users.noreply.github.com>
2025-07-20 23:01:35 +0300

    Created 'Binary Search Tree to Greater Sum Tree' by A2SV Hub

---
+++ 20-Jul-2025/Binary Search Tree to Greater Sum Tree 378222.py
@@ -0,0 +1,24 @@
+# Problem: Binary Search Tree to Greater Sum Tree - https://leetcode.com/problems/binary-search-tree-to-greater-sum-tree/
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def bstToGst(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        greater_sum = 0
+
+        def helper(curr):
+            nonlocal greater_sum
+            if not curr:
+                return
+            helper(curr.right)
+            temp = curr.val
+            curr.val += greater_sum
+            greater_sum += temp
+            helper(curr.left)
+        helper(root)
+        return root 
+      
