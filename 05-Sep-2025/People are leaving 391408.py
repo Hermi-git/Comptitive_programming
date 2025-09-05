ba4b56bd123c1ebfc56186146fed72d56d256401
Hermela Andargie <166540756+Hermi-git@users.noreply.github.com>
2025-09-05 15:49:52 +0300

    Created 'People are leaving' by A2SV Hub

---
+++ 05-Sep-2025/People are leaving 391408.py
@@ -0,0 +1,30 @@
+# Problem: People are leaving - https://codeforces.com/edu/course/2/lesson/7/2/practice/contest/289391/problem/A
+
+n, m = map(int, input().split())
+parent= [i for i in range(n+2)]
+def find(x):
+    while x != parent[x]:
+        parent[x] = parent[parent[x]]
+        x = parent[x]
+    return parent[x]
+
+def union(x, y):
+    rx = find(x)
+    ry = find(y)
+    if rx != ry:
+        parent[rx] = ry 
+
+for _ in range(m):
+    query = input().split()
+
+    if query[0] == "?":
+        person = int(query[1])
+        root = find(person)
+        if root > n:
+            print(-1)
+        else:
+            print(root)
+    elif query[0] == "-":
+        person = int(query[1])
+        union(person, person+1)
+
